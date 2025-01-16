f4cdebcdfe495e769b16c42b95d50b7eff56b5a4
shannmu <shanmu1901@gmail.com>
2025-01-16 15:20:25 +0000

    riscv: add dovetail-aware memory management
    This patch provides the foundation for safely managing memory contexts
    for both in-band and out-of-band tasks in the co-kernel.
    
    Signed-off-by: Tobias Schaffner <tobias.schaffner@siemens.com>

--- arch/riscv/include/asm/mmu_context.h
+++ arch/riscv/include/asm/mmu_context.h
@@ -15,6 +15,8 @@
 
 void switch_mm(struct mm_struct *prev, struct mm_struct *next,
 	struct task_struct *task);
+void switch_oob_mm(struct mm_struct *prev, struct mm_struct *next,
+	struct task_struct *task);
 
 #define activate_mm activate_mm
 static inline void activate_mm(struct mm_struct *prev,
@@ -32,10 +34,6 @@ static inline int init_new_context(struct task_struct *tsk,
 #endif
 	return 0;
 }
-
-static inline void
-switch_oob_mm(struct mm_struct *prev, struct mm_struct *next,
-	      struct task_struct *tsk) { }
 
 DECLARE_STATIC_KEY_FALSE(use_asid_allocator);
 
